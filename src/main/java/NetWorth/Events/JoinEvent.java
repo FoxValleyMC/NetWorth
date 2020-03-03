07b745aa0408c3ed030ddd014d5eada7f3b92d6a
Steve Coulter <stevecoulter99@gmail.com>
2020-03-03 16:38:11 -0600

    unnecessary debug statement

--- src/main/java/NetWorth/Events/JoinEvent.java
+++ src/main/java/NetWorth/Events/JoinEvent.java
@@ -21,12 +21,10 @@ public class JoinEvent implements Listener {
     @EventHandler()
     public void onJoin(PlayerJoinEvent event) {
         PlayerAPI player = (PlayerAPI) event.getPlayer();
-        System.out.println(player.toString());
         String uuid = player.getUniqueId().toString();
         if (Mongo.query(uuid, "uuid") == null) {
             Map<String, Object> objectMap = new HashMap<>();
             objectMap.put("uuid", uuid);
-            objectMap.put("name", player.getName());
             objectMap.put("balance", plugin.getConfig().getInt("default-balance"));
             Mongo.createNew(objectMap);
         }
